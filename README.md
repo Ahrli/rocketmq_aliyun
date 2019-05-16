ac14772cd181198333cee940e100fa4d7cb5b125
ahrli <ahrli@ahrlideMBP.lan>
2019-05-16 21:40:52 +0800

    代码提交

---
+++ README.md
@@ -0,0 +1,30 @@
+# MQ Python HTTP SDK  
+Alyun MQ Documents: http://www.aliyun.com/product/ons
+
+source https://github.com/aliyunmq/mq-http-python-sdk
+
+Aliyun MQ Console: https://ons.console.aliyun.com
+
+Documentation https://libraries.io/pypi/mq-http-sdk
+
+## Requires
+
+Python version: [3.0,3.7]
+
+## Install sdk by pip
+
+1. install pip, see [document](https://pip.pypa.io/en/stable/installing/)
+2. install sdk by pip
+
+```bash
+pip install mq_http_aliyun_sdk
+```
+## Notice
+
+MQClient, Producer, Consumer are not thread safe, please use multi instance in mutli thread.
+
+## Samples
+
+[Publish Message](https://github.com/aliyunmq/mq-http-samples/blob/master/python/producer.py)
+
+[Consume Message](https://github.com/aliyunmq/mq-http-samples/blob/master/python/consumer.py)
---
+++ producer.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# coding=utf8
+
+from mq_http_sdk.mq_exception import MQExceptionBase
+from mq_http_sdk.mq_producer import *
+from mq_http_sdk.mq_client import *
+
+#初始化 client
+mq_client = MQClient(
+    #设置HTTP接入域名（此处以公共云生产环境为例）
+    "xxxxxx",
+    #AccessKey 阿里云身份验证，在阿里云服务器管理控制台创建
+    "xxxxxx",
+    #SecretKey 阿里云身份验证，在阿里云服务器管理控制台创建
+    "xxxxxx"
+    )
+#所属的 Topic
+topic_name = "xxxxxxxx"
+#Topic所属实例ID，默认实例为空None
+instance_id = "xxxxxxxxxxxxxxx"
+
+producer = mq_client.get_producer(instance_id, topic_name)
+
+# 循环发布多条消息
+msg_count = 1
+print ("%sPublish Message To %s\nTopicName:%s\nMessageCount:%s\n" % (10 * "=", 10 * "=", topic_name, msg_count))
+try:
+    for i in range(msg_count):
+        msg_body = "I am test message %s."
+        msg = TopicMessage(
+            # 消息内容
+            "I am test message 3",
+            # 消息标签
+            "1"
+        )
+        re_msg = producer.publish_message(msg)
+        print ("Publish Message Succeed. MessageID:%s, BodyMD5:%s" % (re_msg.message_id, re_msg.message_body_md5))
+        time.sleep(1)
+except MQExceptionBase as e:
+    import traceback
+    print(traceback.format_exc())
+    print ("Publish Message Fail. Exception:%s" % e)
